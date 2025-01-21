5e2511eb699ab7eec828c9e3b2e9f0a8c0a46cc8
iwarley <116743664+iwarley@users.noreply.github.com>
2025-01-21 11:20:25 -0400

    3

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,61 @@
 import express from 'express'
 import mysql from 'mysql2/promise'
 import cors from 'cors'
+import BancoMysql from './db/banco-Mysql'
+import BancoMongo from './db/banco-mongo'
+
 const app = express()
 app.use(express.json())
 app.use(cors())
-app.get("/produtos", async(req,res)=>{
-    //OK -> 0 - Criar o banco de dados e iniciar o servidor de banco.
-    //1 - Criar a conexão com o banco
-    try{
-        const conection = await mysql.createConnection({
-            host:process.env.dbhost?process.env.dbhost:"localhost",
-            user:process.env.dbuser?process.env.dbuser:"root",
-            password:process.env.dbpassword?process.env.dbpassword:"",
-            database:process.env.dbname?process.env.dbname:"banco1022a",
-            port:process.env.dbport?parseInt(process.env.dbport):3306
-        })
-        //2 - Realizar uma consulta na tabela
-        const [result, fields] = await conection.query("SELECT * from produtos")
-        await conection.end()
-        //3 - Devolver os dados pra quem pediu
+
+
+app.get("/produtos", async (req, res) => {
+    try {
+        const banco = new BancoMongo()
+        await banco.criarConexao()
+        const result = await banco.listar()
+        await banco.finalizarConexao()
         res.send(result)
-    }catch(e){
+    } catch (e) {
+        console.log(e)
         res.status(500).send("Server ERROR")
     }
 })
-app.listen(8000,()=>{
+app.post("/produtos", async (req, res) => {
+    try {
+        const {id,nome,descricao,preco,imagem} = req.body
+        const banco = new BancoMongo()
+        await banco.criarConexao()
+        const produto = {id,nome,descricao,preco,imagem}
+        const result = await banco.inserir(produto)
+        await banco.finalizarConexao()
+        res.send(result) 
+    } catch (e) {
+        console.log(e)
+        res.status(500).send(e)
+    }
+})
+
+//DELETAR
+app.delete("/produtos/:id",async(req,res)=>{
+    const banco = new BancoMongo()
+    await banco.criarConexao()
+    const result = await banco.excluir(req.params.id)
+    await banco.finalizarConexao()
+    res.send("Produto excluido com sucesso id: "+req.params.id)
+})
+
+//ALTERAR
+app.put("/produtos/:id",async(req,res)=>{
+    const {id,nome,descricao,preco,imagem} = req.body
+    const produto = {nome,descricao,preco,imagem}
+    const banco = new BancoMongo()
+    await banco.criarConexao()
+    const result = await banco.alterar(req.params.id,produto)
+    await banco.finalizarConexao()
+    res.send("Produto alterado com sucesso id: "+req.params.id)
+})
+
+app.listen(8000, () => {
     console.log("Iniciei o servidor")
 })
